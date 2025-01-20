0a4f36f31c50f645ebd7a558386f9eeae2eb018e
no-nonsense-basketball <justinho327@gmail.com>
2025-01-20 23:05:54 +0000

    Deploying to gh-pages from @ no-nonsense-basketball/no-nonsense-basketball.github.io@d69892cb85ea685c923a0c792fe071799838d80d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -384,11 +384,11 @@ ninja.data = [{
           window.location.href = "/blog/2015/math/";
         
       },
-    },{id: "post-a-post-with-code",
+    },{id: "post-step-by-step-guide-to-structuring-workouts",
       
-        title: "a post with code",
+        title: "Step by Step Guide to Structuring Workouts",
       
-      description: "an example of a blog post with some code",
+      description: "Stop wasting time when you work out. Read this guide.",
       section: "Posts",
       handler: () => {
         
